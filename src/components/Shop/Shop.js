50d790a70f030b61d6d26925604be9f2e24de9d3
H_A _Opu <wwwhasib4@gmail.com>
2022-03-25 22:14:40 +0600

    fetch and map on watch.json fake-data

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Shop.css";
 const Shop = () => {
+  const [watches, setWatches] = useState([]);
+  useEffect(() => {
+    fetch("Watch.json")
+      .then((res) => res.json())
+      .then((data) => setWatches(data));
+  }, []);
   return (
-    <div>
-      <div className="watch-container">
-        <h3>Watch container</h3>
+    <div className="row w-100 mx-auto">
+      <div className="watch-container col-12 col-lg-9 bg-warning">
+        <h3>Watch container {watches.length}</h3>
+        {watches.map((watch) => console.log(watch))}
       </div>
-      <div className="cart-container">
+      <div className="cart-container col-12 col-lg-3 bg-info">
         <h4>Cart container</h4>
       </div>
     </div>
